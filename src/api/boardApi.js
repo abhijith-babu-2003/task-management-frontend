5fe087a3cdf952d58a583207be313d92d9ee162f
git practice <abhijith7133@gmail.com>
2025-08-22 17:39:19 +0530

    fix isssues

--- src/api/boardApi.js
+++ src/api/boardApi.js
@@ -1,7 +1,8 @@
 // src/api/boardApi.js
 import axios from 'axios';
 
-export const API_URL = 'http://localhost:5000/api';
+
+export const API_URL = import.meta.env.VITE_API_URL+'/api'
 
 const api = axios.create({
   baseURL: API_URL,
--- src/api/coulmnApi.js
+++ src/api/coulmnApi.js
@@ -1,7 +1,8 @@
 
 import axios from 'axios';
 
-export const API_URL = 'http://localhost:5000/api';
+
+export const API_URL = import.meta.env.VITE_API_URL+'/api'
 
 const api = axios.create({
   baseURL: API_URL,
